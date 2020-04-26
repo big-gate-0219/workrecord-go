1bfeae25fefd037b657a104d62ec3f55f9da6295
big.gate.0219 <big.gate.0219@gmail.com>
2020-04-26 18:46:49 +0900

    ユーザ一覧取得API

---
+++ src/web/api/fetch_accounts.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"github.com/labstack/echo"
+	"github.com/valyala/fasthttp"
+	"middlewares"
+	"models"
+)
+
+type FetchAccountsResponse struct {
+	Status string `json:"status"`
+	Users  []models.User
+}
+
+func FetchAccounts() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
+
+		query := c.QueryParam("q")
+		users := []models.User{}
+		dbs.DB.Where("uid like ?", "%"+query+"%").Find(&users)
+
+		response := FetchAccountsResponse{
+			Status: "SUCCESS",
+			Users:  users,
+		}
+		return c.JSON(fasthttp.StatusOK, response)
+	}
+
+}
