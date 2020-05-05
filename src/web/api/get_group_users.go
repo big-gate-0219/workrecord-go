093adac6d8993799619eb268b862db2cf7004655
big.gate.0219 <big.gate.0219@gmail.com>
2020-05-05 09:13:28 +0900

    入力チェックを追加(get_group_users)

--- src/web/api/get_group_users.go
+++ src/web/api/get_group_users.go
@@ -1,23 +1,30 @@
 package api
 
 import (
-	"strconv"
 	"github.com/labstack/echo"
 	"github.com/valyala/fasthttp"
-	"models"
 	"middlewares"
+	"models"
+	"strconv"
+	"validate"
 )
 
 type GetGroupUsersResponse struct {
-	Status string `json:"status"`
-	Users []models.User `json:"users"`
+	Status string        `json:"status"`
+	Users  []models.User `json:"users"`
 }
 
 func GetGroupUsers() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		groupId, _ := strconv.ParseUint(c.Param("group_id"), 10, 64)
-
 		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
+
+		groupId, _ := strconv.ParseUint(c.Param("group_id"), 10, 64)
+		g := models.Group{}
+		if dbs.DB.Where(&models.Group{ID: groupId}).First(&g).RecordNotFound() {
+			validateError := validate.CreateSingleErrors("not_found", "group_id")
+			errResponse := validate.CreateErrorResponse(validateError)
+			return c.JSON(fasthttp.StatusBadRequest, errResponse)
+		}
 
 		group := models.Group{}
 		dbs.DB.Where(models.Group{ID: groupId}).Find(&group)
@@ -32,9 +39,9 @@ func GetGroupUsers() echo.HandlerFunc {
 			users = append(users, user)
 		}
 
-		response := GetGroupUsersResponse {
+		response := GetGroupUsersResponse{
 			Status: "SUCCESS",
-			Users: users,
+			Users:  users,
 		}
 		return c.JSON(fasthttp.StatusOK, response)
 	}
