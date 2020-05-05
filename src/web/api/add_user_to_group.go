8e3bf05e5ece4ceeb08c79ecdd643a2d6c5bcae3
big.gate.0219 <big.gate.0219@gmail.com>
2020-05-05 09:00:44 +0900

    入力チェックを追加(add_user_to_group)

--- src/web/api/add_user_to_group.go
+++ src/web/api/add_user_to_group.go
@@ -1,15 +1,17 @@
 package api
 
 import (
-	"strconv"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
 	"github.com/valyala/fasthttp"
-	"models"
 	"middlewares"
+	"models"
+	"strconv"
+	"validate"
 )
 
 type AddUserToGroupRequest struct {
-	UserId uint64 `json:"user_id"`
+	UserId uint64 `json:"user_id" validate:"required"`
 }
 
 func AddUserToGroup() echo.HandlerFunc {
@@ -17,24 +19,34 @@ func AddUserToGroup() echo.HandlerFunc {
 		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
 
 		groupId, _ := strconv.ParseUint(c.Param("group_id"), 10, 64)
+
 		request := new(AddUserToGroupRequest)
 		if err := c.Bind(request); err != nil {
 			return err
 		}
+		if err := c.Validate(request); err != nil {
+			validateError := validate.TranslateError(err.(validator.ValidationErrors))
+			errResponse := validate.CreateErrorResponse(validateError)
+			return c.JSON(fasthttp.StatusBadRequest, errResponse)
+		}
 
 		group := models.Group{}
 		if dbs.DB.Where(models.Group{ID: groupId}).First(&group).RecordNotFound() {
-			return c.JSON(fasthttp.StatusInternalServerError, "Not found group.")
+			validateError := validate.CreateSingleErrors("not_found", "groupId")
+			errResponse := validate.CreateErrorResponse(validateError)
+			return c.JSON(fasthttp.StatusBadRequest, errResponse)
 		}
 
 		user := models.User{}
 		if dbs.DB.Where(models.User{ID: request.UserId}).First(&user).RecordNotFound() {
-			return c.JSON(fasthttp.StatusInternalServerError, "Not found user.")
+			validateError := validate.CreateSingleErrors("not_found", "user_id")
+			errResponse := validate.CreateErrorResponse(validateError)
+			return c.JSON(fasthttp.StatusBadRequest, errResponse)
 		}
 
-		groupUser := models.GroupUser {
+		groupUser := models.GroupUser{
 			GroupId: group.ID,
-			UserId: user.ID,
+			UserId:  user.ID,
 		}
 		dbs.DB.Create(&groupUser)
 
