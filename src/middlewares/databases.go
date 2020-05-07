307219e782e88c6de22dc55491281da57042369d
big.gate.0219 <big.gate.0219@gmail.com>
2020-05-08 08:43:58 +0900

    DBミドルウェアにデータベーストランザクションを追加

--- src/middlewares/databases.go
+++ src/middlewares/databases.go
@@ -2,9 +2,9 @@ package middlewares
 
 import (
 	"databases"
-
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 )
 
 type DatabaseClient struct {
@@ -16,14 +16,26 @@ func DatabasesService() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			session, _ := databases.Connect()
 			d := DatabaseClient{DB: session}
+			tx := d.DB.Begin()
+
 			defer d.DB.Close()
 
 			d.DB.LogMode(true)
 			c.Set("dbs", &d)
+			c.Set("tx", &tx)
 			if err := next(c); err != nil {
+				tx.Rollback()
+				logrus.Debug("Transction Rollback: ", err)
 				return err
 			}
+			if c.Response().Status != 200 {
+				tx.Rollback()
+				logrus.Debug("Transction Rollback Status: ", c.Response().Status)
+				return nil
+			}
 
+			logrus.Debug("Transaction Commit")
+			tx.Commit()
 			return nil
 		}
 	}
