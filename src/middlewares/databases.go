80c7ac7da564b4ff2d58a5944386ffa61aa29647
big.gate.0219 <big.gate.0219@gmail.com>
2020-05-13 09:51:28 +0900

    データベース・ミドルウェアでのトランザクションの保持方法を変更

--- src/middlewares/databases.go
+++ src/middlewares/databases.go
@@ -8,34 +8,36 @@ import (
 )
 
 type DatabaseClient struct {
-	DB *gorm.DB
+	DB          *gorm.DB
+	Transaction *gorm.DB
 }
 
 func DatabasesService() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			session, _ := databases.Connect()
-			d := DatabaseClient{DB: session}
-			tx := d.DB.Begin()
+			d := DatabaseClient{
+				DB:          session,
+				Transaction: session.Begin(),
+			}
 
 			defer d.DB.Close()
 
 			d.DB.LogMode(true)
 			c.Set("dbs", &d)
-			c.Set("tx", &tx)
 			if err := next(c); err != nil {
-				tx.Rollback()
+				d.Transaction.Rollback()
 				logrus.Debug("Transction Rollback: ", err)
 				return err
 			}
 			if c.Response().Status != 200 {
-				tx.Rollback()
+				d.Transaction.Rollback()
 				logrus.Debug("Transction Rollback Status: ", c.Response().Status)
 				return nil
 			}
 
 			logrus.Debug("Transaction Commit")
-			tx.Commit()
+			d.Transaction.Commit()
 			return nil
 		}
 	}
